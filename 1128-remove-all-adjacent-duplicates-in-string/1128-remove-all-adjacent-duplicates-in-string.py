cc0ddaeb402f2438717e036c1874f578738ede14
Amlan Alok <amlanalok@gmail.com>
2025-08-01 15:44:10 -0500

    Time: 24 ms (33.51%), Space: 18.2 MB (97.22%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        return aug01_25(s)
+
+def aug01_25(s):
+    '''TC = n, SC = n'''
+    stack = deque()
+
+    for c in s:
+        if stack:
+            if stack[-1] == c:
+                stack.pop()
+                continue
+        stack.append(c)
+
+    return ''.join(stack)
+
