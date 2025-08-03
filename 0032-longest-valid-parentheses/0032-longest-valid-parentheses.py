24f7f65212288d51ae5c48281fecfe14d659e25e
Amlan Alok <amlanalok@gmail.com>
2025-08-02 21:20:51 -0500

    Time: 7 ms (58.22%), Space: 19.1 MB (5.5%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -19,7 +19,8 @@ def aug02_25(s):
     return 0
 
 def aug01_25(s):
-    '''TC = n, SC = n'''
+    '''Stack Approach — TC = n, SC = n'''
+    
     stack = deque()
     stack.append(-1)
     maxlen = 0
