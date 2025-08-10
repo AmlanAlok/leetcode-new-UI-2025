4bf52fdcb770f718495e39fd6c830672473f0f5a
Amlan Alok <amlanalok@gmail.com>
2025-08-10 17:15:16 -0500

    Time: 3 ms (7.74%), Space: 18 MB (13.08%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return aug09_25(root)
+        return aug10_25(root)
 
 def aug09_25(root):
     '''Recursive, TC = n, SC = n where n = total nodes in the tree'''
@@ -28,9 +28,34 @@ def ismirror(x, y):
 '''
 [1,2,2,3,4,4,3]
 [1,2,2,null,3,null,3]
+[1,2,3]
+[9,-42,-42,null,76,76,null,null,13,null,13]
 '''
+from collections import deque
 
+def aug10_25(root):
+    '''Iterative, TC = n, SC = n'''
+    q = deque()
+    q.append(root)
+    q.append(root)
+
+    while q:
+        x = q.popleft()
+        y = q.popleft()
+
+        if x is None and y is None:
+            continue
+        if x is None or y is None:
+            print(x, y)
+            return False
+        if x.val != y.val:
+            print('p', x, y)
+            return False
+        
+        q.append(x.left)
+        q.append(y.right)
+        q.append(x.right)
+        q.append(y.left)
     
-
-
+    return True
         
