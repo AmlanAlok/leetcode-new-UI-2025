10d9fe137e78bb5eed2a715543bcf76c23eeabcc
Amlan Alok <amlanalok@gmail.com>
2025-08-11 00:15:51 -0500

    Time: 0 ms (100%), Space: 17.9 MB (31.6%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -0,0 +1,82 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        return aug10_25(root)
+
+def aug10_25(root):
+    
+    if root:
+        root.left, root.right = aug10_25(root.right), aug10_25(root.left)
+
+    return root
+
+'''
+[2,1,3]
+[4,2,7,1,3,6,9]
+[]
+'''
+
+
+    
+def ans1(root):
+    
+    if root:
+        
+        # if root.left is None and root.right is None:
+        #     return root
+        
+
+        l = ans1(root.left)
+        r = ans1(root.right)
+        root.right, root.left = l, r
+         # if root.left:
+        #     node = ans1(root.left)
+        #     temp = root.left
+        #     root.left = root.right
+        #     root.right = temp
+        # if root.right:
+        #     node = ans1(root.right)
+        #     temp = root.left
+        #     root.left = root.right
+        #     root.right = temp
+    return root
+
+def ans2(root):
+    
+    # if not root:
+    if root is None:
+        return None
+    right = ans2(root.right)
+    left = ans2(root.left)
+    root.left = right
+    root.right = left
+    
+    return root
+
+def e1(root):
+    
+    if root.left is None and root.right is None:        # this lines fails in [] case
+        return
+    if root.left:
+        p1(root.left)
+    if root.right:
+        p1(root.right)
+    
+    root.left, root.right = root.right, root.left
+    
+    return root
+
+'''
+Best answer yet
+'''
+def p1(root):
+    
+    if root:
+        root.left, root.right = p1(root.right), p1(root.left)
+    return root
+    
