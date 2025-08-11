8edd84ec7e857796385b0cb8e38c13b33ef8642b
Amlan Alok <amlanalok@gmail.com>
2025-08-10 20:26:10 -0500

    Time: 0 ms (100%), Space: 18.3 MB (13.13%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,6 +1,7 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        return aug08_25(image, sr, sc, color)
+        # return aug08_25(image, sr, sc, color)
+        return aug10_25(image, sr, sc, color)
 
 def aug08_25(image, sr, sc, color):
     '''
@@ -41,3 +42,28 @@ If newColor == color, the DFS will keep revisiting the same pixels and flipping 
 This causes infinite recursion, eventually leading to a stack overflow (or a RecursionError in Python).
 '''
 
+def aug10_25(image, sr, sc, color):
+
+    R, C = len(image), len(image[0])
+    cur = image[sr][sc]
+
+    if cur == color:
+        return image
+
+    def dfs(r, c):
+        if image[r][c] == cur:
+            image[r][c] = color
+
+            if r > 0:
+                dfs(r-1, c)
+            if r+1 < R:
+                dfs(r+1, c)
+            if c > 0:
+                dfs(r, c-1)
+            if c+1 < C:
+                dfs(r, c+1)
+            
+    dfs(sr, sc)
+
+    return image
+    
