595a25f39f9807ae56793c4f1942675a60aa9f47
Amlan Alok <amlanalok@gmail.com>
2025-08-21 19:01:45 -0500

    Time: 77 ms (22.81%), Space: 18 MB (56.01%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,6 +1,27 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        return ans(s)
+        return aug21_25(s)
+
+def aug21_25(s):
+    '''TC = n, SC = n'''
+    d = {}
+
+    for i, c in enumerate(s):
+        
+        if c not in d:
+            d[c] = [i, 1]
+        else:
+            temp = d[c]
+            temp[1] += 1
+            d[c] = temp
+
+    for k, v in d.items():
+        if v[1] == 1:
+            return v[0]
+
+    return -1
+
+
     
 def ans(s):
     '''Tc=n, SC=26=1'''
